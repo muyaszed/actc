943bad8e8293c9839a0ae65049a40c3280a75a6a
yaz-basekit <yaz-basekit@yaz-basekits.local>
2023-09-17 19:51:39 +0100

    Added bin command script

--- src/main.ts
+++ src/main.ts
@@ -13,75 +13,74 @@ let fileContent: string;
 const outputFolder = args[2] || './output';
 const testName = args[3] || 'test'
 
+export const main = () => {
+    try {
 
-
-
-
-try {
-
-    // Argument validation
-    if (!folderName) {
-        throw new Error('Need to specify the XMl folder location');
-    }
-
-    const xmlFile = readFileSync(`${process.cwd()}${folderName}/${fileName}.xml`, 'utf8');
-    const parser = new XMLParser();
-    const json = parser.parse(xmlFile);
-    const dom = new JSDOM(json.rss.channel.item.description);
-    const allItems = [...dom.window.document.querySelectorAll("p")].slice(1);
-    const includedItems = allItems.filter(item => {
-        const str = item.textContent;
-
-        if (!str) {
-            return false;
+        // Argument validation
+        if (!folderName) {
+            throw new Error('Need to specify the XMl folder location');
         }
-        
-        return str.includes('Given');
-    }).map(includedItem =>  {
-        console.log(includedItem.innerHTML)
-        return includedItem.innerHTML
-    });
-    console.log(includedItems);
-    const itemsSorted = includedItems.map(item => {
-        const splitLineItems = item.split('<br>\n');
-        console.log('SpliLine Item', splitLineItems);
-        const newObj: Record<string, string> = {};
-        splitLineItems.forEach(lineItem => {
-            const splitWord = lineItem.split(' ');
-            const subject = splitWord[0];
-            const description = splitWord.slice(1).join(' ');
-            newObj[subject.toLowerCase()] = description;
-        })
-
-        return newObj;
-    })
-
-    fileContent = [
-        "import { test } from '@playwright/test';",
-        '',
-        `test.describe('${json.rss.channel.item.title}', () => {`,
-        itemsSorted.map(item => (
-            [
-                `   test.describe('${item.given}${item.and ? `and ${item.and}` : ''}', () => {`,
-                `       test.describe('${item.when}', () => {`,
-                `           test('${item.then}');`,
-                '       });',
-                '   });'
-            ].join('\n')
-        )).join('\n'),
-        '});',
-        '',
-    ].join('\n');
     
-    if (!existsSync(outputFolder)) {
-        mkdirSync(outputFolder, { recursive: true });
+        const xmlFile = readFileSync(`${process.cwd()}${folderName}/${fileName}.xml`, 'utf8');
+        const parser = new XMLParser();
+        const json = parser.parse(xmlFile);
+        const dom = new JSDOM(json.rss.channel.item.description);
+        const allItems = [...dom.window.document.querySelectorAll("p")].slice(1);
+        const includedItems = allItems.filter(item => {
+            const str = item.textContent;
+    
+            if (!str) {
+                return false;
+            }
+            
+            return str.includes('Given');
+        }).map(includedItem =>  {
+            console.log(includedItem.innerHTML)
+            return includedItem.innerHTML
+        });
+        console.log(includedItems);
+        const itemsSorted = includedItems.map(item => {
+            const splitLineItems = item.split('<br>\n');
+            console.log('SpliLine Item', splitLineItems);
+            const newObj: Record<string, string> = {};
+            splitLineItems.forEach(lineItem => {
+                const splitWord = lineItem.split(' ');
+                const subject = splitWord[0];
+                const description = splitWord.slice(1).join(' ');
+                newObj[subject.toLowerCase()] = description;
+            })
+    
+            return newObj;
+        })
+    
+        fileContent = [
+            "import { test } from '@playwright/test';",
+            '',
+            `test.describe('${json.rss.channel.item.title}', () => {`,
+            itemsSorted.map(item => (
+                [
+                    `   test.describe('${item.given}${item.and ? `and ${item.and}` : ''}', () => {`,
+                    `       test.describe('${item.when}', () => {`,
+                    `           test('${item.then}');`,
+                    '       });',
+                    '   });'
+                ].join('\n')
+            )).join('\n'),
+            '});',
+            '',
+        ].join('\n');
+        
+        if (!existsSync(outputFolder)) {
+            mkdirSync(outputFolder, { recursive: true });
+        }
+    
+        writeFileSync(`${outputFolder}/${testName}.tsx`, fileContent);
+        console.log('Test file created successfully');
+    } catch(error: unknown) {
+        if (error instanceof Error) {
+            console.log(error.message);
+        }
     }
+};
 
-    writeFileSync(`${outputFolder}/${testName}.tsx`, fileContent);
-    console.log('Test file created successfully');
-} catch(error: unknown) {
-    if (error instanceof Error) {
-        console.log(error.message);
-    }
-}
 
