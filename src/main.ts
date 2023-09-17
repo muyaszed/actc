8f7742ac9fa153a7ff044451c98f0b5b9186b65b
yaz-basekit <yaz-basekit@yaz-basekits.local>
2023-09-17 18:28:50 +0100

    Added typescript

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,28 @@ import { XMLParser } from 'fast-xml-parser';
 import { readFileSync, writeFileSync, mkdirSync, existsSync } from 'fs';
 import { JSDOM } from 'jsdom';
 
-const args = process.argv.slice(2);
+type Arguments = (string | null)[];
+type ok = string | null;
+
+
+const args: Arguments = process.argv.slice(2);
 const folderName = args[0];
 const fileName = args[1];
-let fileContent;
+let fileContent: string;
 const outputFolder = args[2] || './output';
 const testName = args[3] || 'test'
 
+
+
+
+
 try {
+
+    // Argument validation
+    if (!folderName) {
+        throw new Error('Need to specify the XMl folder location');
+    }
+
     const xmlFile = readFileSync(`${process.cwd()}${folderName}/${fileName}.xml`, 'utf8');
     const parser = new XMLParser();
     const json = parser.parse(xmlFile);
@@ -17,13 +31,21 @@ try {
     const allItems = [...dom.window.document.querySelectorAll("p")].slice(1);
     const includedItems = allItems.filter(item => {
         const str = item.textContent;
+
+        if (!str) {
+            return false;
+        }
         
         return str.includes('Given');
-    }).map(includedItem => includedItem.textContent);
-    
+    }).map(includedItem =>  {
+        console.log(includedItem.innerHTML)
+        return includedItem.innerHTML
+    });
+    console.log(includedItems);
     const itemsSorted = includedItems.map(item => {
-        const splitLineItems = item.split('\n');
-        const newObj = {};
+        const splitLineItems = item.split('<br>\n');
+        console.log('SpliLine Item', splitLineItems);
+        const newObj: Record<string, string> = {};
         splitLineItems.forEach(lineItem => {
             const splitWord = lineItem.split(' ');
             const subject = splitWord[0];
@@ -40,7 +62,7 @@ try {
         `test.describe('${json.rss.channel.item.title}', () => {`,
         itemsSorted.map(item => (
             [
-                `   test.describe('${item.given} ${item.and ? `and ${item.and}` : ''}' , () => {`,
+                `   test.describe('${item.given}${item.and ? `and ${item.and}` : ''}', () => {`,
                 `       test.describe('${item.when}', () => {`,
                 `           test('${item.then}');`,
                 '       });',
@@ -49,22 +71,17 @@ try {
         )).join('\n'),
         '});',
         '',
-    ].join('\n');    
-} catch(error) {
-    if (error.code === 'ENONENT') {
-        console.log('Fiile not found');
-    } else {
-        console.log('Something not right', error);
-    }
-}
-
-try {
+    ].join('\n');
+    
     if (!existsSync(outputFolder)) {
-        mkdirSync(outputFolder, { recursice: true });
+        mkdirSync(outputFolder, { recursive: true });
     }
 
     writeFileSync(`${outputFolder}/${testName}.tsx`, fileContent);
     console.log('Test file created successfully');
-} catch(error) {
-    console.log(error)
+} catch(error: unknown) {
+    if (error instanceof Error) {
+        console.log(error.message);
+    }
 }
+
