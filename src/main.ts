d4816076bbe7ba4fe1ac020b5211392619b3bcf2
yaz-basekit <yaz-basekit@yaz-basekits.local>
2023-09-18 06:52:56 +0100

    Refactor main function

--- src/main.ts
+++ src/main.ts
@@ -3,62 +3,80 @@ import { readFileSync, writeFileSync, mkdirSync, existsSync } from 'fs';
 import { JSDOM } from 'jsdom';
 import config from 'config';
 
-let fileContent: string;
+type GWT = 'given' | 'when' | 'then' | 'and';
+type SortedGWT = Record<GWT, string>;
+
 const xmlFolderName = config.get('xml.folder');
 const xmlFileName = config.get('xml.file');
 const outputFolder = config.get('output.folder');
 const outputFile = config.get('output.file');
 
+const includeACRelatedStringOnly = (xmlFile: string) => {
+    const parser = new XMLParser();
+    const json = parser.parse(xmlFile);
+    const dom = new JSDOM(json.rss.channel.item.description);
+    const allItems = [...dom.window.document.querySelectorAll("p")].slice(1);
+
+    return ({
+        items:  allItems
+            .filter(item => {
+                const str = item.textContent;
+
+                if (!str) {
+                    return false;
+                }
+                
+                return str.includes('Given');
+            })
+            .map(includedItem => includedItem.innerHTML),
+        json,
+    });
+};
+
+const sortGWT = (listOfClauses: string[]): SortedGWT[] => (
+    listOfClauses.map(item => {
+        const splitLineItems = item.split('<br>\n');
+        const newObj: SortedGWT = {
+            given: '',
+            when: '',
+            then: '',
+            and: '',
+        };
+        splitLineItems.forEach(lineItem => {
+            const splitWord = lineItem.split(' ');
+            const subject = splitWord[0];
+            const description = splitWord.slice(1).join(' ');
+            newObj[subject.toLowerCase() as GWT] = description;
+        })
+
+        return newObj;
+    })
+);
+
+const generateTestCases = (sortedGWT: SortedGWT[], sourceJSON: any) => (
+    [
+        `test.describe('${sourceJSON.rss.channel.item.title}', () => {`,
+        sortedGWT.map(item => (
+            [
+                `   test.describe('${item.given}${item.and ? `and ${item.and}` : ''}', () => {`,
+                `       test.describe('${item.when}', () => {`,
+                `           test('${item.then}');`,
+                '       });',
+                '   });'
+            ].join('\n')
+        )).join('\n'),
+        '});',
+        '',
+    ].join('\n')
+);
+
 export const main = () => {
     try {    
         const xmlFile = readFileSync(`${process.cwd()}${xmlFolderName}/${xmlFileName}`, 'utf8');
-        const parser = new XMLParser();
-        const json = parser.parse(xmlFile);
-        const dom = new JSDOM(json.rss.channel.item.description);
-        const allItems = [...dom.window.document.querySelectorAll("p")].slice(1);
-        const includedItems = allItems.filter(item => {
-            const str = item.textContent;
+        const { items: includedItems, json } = includeACRelatedStringOnly(xmlFile);
+        const itemsSorted = sortGWT(includedItems);
     
-            if (!str) {
-                return false;
-            }
-            
-            return str.includes('Given');
-        }).map(includedItem =>  {
-            console.log(includedItem.innerHTML)
-            return includedItem.innerHTML
-        });
-        console.log(includedItems);
-        const itemsSorted = includedItems.map(item => {
-            const splitLineItems = item.split('<br>\n');
-            console.log('SpliLine Item', splitLineItems);
-            const newObj: Record<string, string> = {};
-            splitLineItems.forEach(lineItem => {
-                const splitWord = lineItem.split(' ');
-                const subject = splitWord[0];
-                const description = splitWord.slice(1).join(' ');
-                newObj[subject.toLowerCase()] = description;
-            })
-    
-            return newObj;
-        })
-    
-        fileContent = [
-            "import { test } from '@playwright/test';",
-            '',
-            `test.describe('${json.rss.channel.item.title}', () => {`,
-            itemsSorted.map(item => (
-                [
-                    `   test.describe('${item.given}${item.and ? `and ${item.and}` : ''}', () => {`,
-                    `       test.describe('${item.when}', () => {`,
-                    `           test('${item.then}');`,
-                    '       });',
-                    '   });'
-                ].join('\n')
-            )).join('\n'),
-            '});',
-            '',
-        ].join('\n');
+        const fileContent = generateTestCases(itemsSorted, json);
         
         if (!existsSync(outputFolder as string)) {
             mkdirSync(outputFolder as string, { recursive: true });
